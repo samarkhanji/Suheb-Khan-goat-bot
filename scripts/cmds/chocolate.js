df6560ce01b78505145ae4e7a2d9c81167942879
Raj-0789 <reporttrick63@gmail.com>
2025-02-09 10:57:56 +0530

    Update chocolate.js

--- scripts/cmds/chocolate.js
+++ scripts/cmds/chocolate.js
@@ -1,31 +1,22 @@
-const fs = require("fs");
-
-const config = {
-  name: "chocolate",
-  version: "1.0.1",
-  hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-  description: "hihihihi",
-  commandCategory: "no prefix",
-  usages: "chocolate",
-  cooldowns: 5,
-  usePrefix: false
-};
-
-async function onStart({ api, event }) {
-  var { threadID, messageID } = event;
-  if (event.body.indexOf("Chocolate") == 0 || event.body.indexOf("chocolate") == 0 || event.body.indexOf("toffee") == 0 || event.body.indexOf("Toffee") == 0) {
-    var msg = {
-      body: "Ye lo chocolate ",
-      attachment: fs.createReadStream(__dirname + `/assets/image/chocolate.jpg`)
-    }
-    api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("", event.messageID, (err) => { }, true)
-  }
-}
-
 module.exports = {
-  config,
-  onStart,
-  run: onStart
-};
+ config: {
+	 name: "CHOCOLATE",
+	 version: "1.0",
+	 author: "AceGun",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "chocolate") {
+ return message.reply({
+ body: "Ye Baby Aapke Liye Chocolate Kha Lo 😋😋",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/qxusJJr.jpeg")
+ });
+ }
+ }
+}
