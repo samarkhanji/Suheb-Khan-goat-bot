7ec7146bb238b43dd9aae7f55661a8f8f949b9f6
Raj-0789 <reporttrick63@gmail.com>
2025-02-09 14:18:27 +0530

    Update chocolate.js

--- scripts/cmds/chocolate.js
+++ scripts/cmds/chocolate.js
@@ -1,39 +1,46 @@
-const fs = require("fs");
+const fs = require("fs-extra");
+const path = require("path");
 
 module.exports = {
-  config: {
-    name: "Chocolate",
-    version: "1.0",
-    author: "Priyansh Rajput",
-    countDown: 2,
-    role: 0,
-    description: "Sends a chocolate image when triggered.",
-    category: "box chat",
-  },
+    config: {
+        name: "chocolate",
+        version: "1.0",
+        author: "Priyansh Rajput",
+        countDown: 5,
+        role: 0,
+        description: "Detects sweet-related keywords and sends a message with a photo.",
+        category: "fun",
+        guide: {
+            vi: "{pn} chocolate: Gửi thông điệp về chocolate với ảnh.",
+            en: "{pn} chocolate: Send a message about chocolate with a photo."
+        }
+    },
 
-  handleEvent: function ({ api, event }) {
-    var { threadID, messageID, body } = event;
-    if (["Chocolate", "chocolate", "Toffe", "toffe"].some((v) => body && body.startsWith(v))) {
-      const imagePath = __dirname + "/tmp/chocolate.jpg";
-      
-      if (fs.existsSync(imagePath)) {
-        var msg = {
-          body: "Ye lo chocolate 🍫",
-          attachment: fs.createReadStream(imagePath),
-        };
-        api.sendMessage(msg, threadID, (err, info) => {
-          if (!err) {
-            api.setMessageReaction("🍫", info.messageID, () => {}, true);
-          }
-        });
-      } else {
-        console.error('The file "chocolate.jpg" does not exist in the cache directory.');
-        api.sendMessage({ body: "An error occurred while trying to send the chocolate image." }, threadID);
-      }
+    langs: {
+        vi: {
+            replyMessage: "Đây là chocolate 🍫 dành cho bạn!",
+        },
+        en: {
+            replyMessage: "Here's some chocolate 🍫 for you!",
+        }
+    },
+
+    onStart: async function ({ message, event, getLang }) {
+        const sweetWords = ["chocolate", "toffee", "candy"];
+        const userMessage = event.body.toLowerCase();
+
+        // Check if the user's message contains any sweet-related word
+        if (sweetWords.some(word => userMessage.includes(word))) {
+            // Define the path to the chocolate.jpg image in the cache folder
+            const imagePath = path.join(__dirname, "cache", "chocolate.jpg");
+
+            // Check if the image exists
+            if (fs.existsSync(imagePath)) {
+                // Send the message and the image
+                return message.reply(getLang("replyMessage"), { attachment: fs.createReadStream(imagePath) });
+            } else {
+                return message.reply("Sorry, I couldn't find the chocolate image.");
+            }
+        }
     }
-  },
-
-  onStart: function ({ api, event }) {
-    // This function is currently unused but can be utilized for initialization logic if needed.
-  },
 };
