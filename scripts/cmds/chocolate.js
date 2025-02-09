35c87bebacf2d6a010c0c40818d9a346347f4767
Raj-0789 <reporttrick63@gmail.com>
2025-02-09 13:31:01 +0530

    Update chocolate.js

--- scripts/cmds/chocolate.js
+++ scripts/cmds/chocolate.js
@@ -1,22 +1,39 @@
-module.exports = {
- config: {
-	 name: "CHOCOLATE",
-	 version: "1.0",
-	 author: "AceGun",
-	 countDown: 5,
-	 role: 0,
-	 shortDescription: "no prefix",
-	 longDescription: "no prefix",
-	 category: "no prefix",
- },
+const fs = require("fs");
 
- onStart: async function(){}, 
- onChat: async function({ event, message, getLang }) {
- if (event.body && event.body.toLowerCase() === "chocolate") {
- return message.reply({
- body: "Ye Baby Aapke Liye Chocolate Kha Lo 😋😋",
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/qxusJJr.jpeg")
- });
- }
- }
-}
+module.exports = {
+  config: {
+    name: "Chocolate",
+    version: "1.0",
+    author: "Priyansh Rajput",
+    countDown: 2,
+    role: 0,
+    description: "Sends a chocolate image when triggered.",
+    category: "box chat",
+  },
+
+  handleEvent: function ({ api, event }) {
+    var { threadID, messageID, body } = event;
+    if (["Chocolate", "chocolate", "Toffe", "toffe"].some((v) => body && body.startsWith(v))) {
+      const imagePath = __dirname + "/tmp/chocolate.jpg";
+      
+      if (fs.existsSync(imagePath)) {
+        var msg = {
+          body: "Ye lo chocolate 🍫",
+          attachment: fs.createReadStream(imagePath),
+        };
+        api.sendMessage(msg, threadID, (err, info) => {
+          if (!err) {
+            api.setMessageReaction("🍫", info.messageID, () => {}, true);
+          }
+        });
+      } else {
+        console.error('The file "chocolate.jpg" does not exist in the cache directory.');
+        api.sendMessage({ body: "An error occurred while trying to send the chocolate image." }, threadID);
+      }
+    }
+  },
+
+  onStart: function ({ api, event }) {
+    // This function is currently unused but can be utilized for initialization logic if needed.
+  },
+};
