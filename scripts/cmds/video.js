df3abce244b677d70047c49cf8988a20d957abbd
Raj-0789 <reporttrick63@gmail.com>
2025-03-10 10:09:01 +0530

    Update video.js

--- scripts/cmds/video.js
+++ scripts/cmds/video.js
@@ -9,7 +9,7 @@ module.exports = {
     version: "1.0.0",
     author: "aayuse",
     countDown: 5,
-    role: 2,
+    role: 0,
     shortDescription: {
       en: "Download YouTube videos (under 25MB) or provide a link",
     },
