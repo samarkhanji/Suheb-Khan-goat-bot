3478c8c9a970201337e4fc4d413d727a62dfe5ef
Raj-0789 <reporttrick63@gmail.com>
2025-03-18 13:52:03 +0530

    Update fun.js

--- scripts/cmds/fun.js
+++ scripts/cmds/fun.js
@@ -1,3 +1,4 @@
+const { GoatWrapper } = require("fca-liane-utils");
 const axios = require('axios');
 
 module.exports = {
@@ -316,3 +317,5 @@ module.exports = {
     }
   },
 };
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
