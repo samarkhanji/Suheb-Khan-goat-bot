6401449caa79e557b13bd7b2a75be7c98913bc93
Raj-0789 <reporttrick63@gmail.com>
2025-03-18 13:53:18 +0530

    Update blue.js

--- scripts/cmds/blue.js
+++ scripts/cmds/blue.js
@@ -1,3 +1,4 @@
+const { GoatWrapper } = require("fca-liane-utils");
 module.exports = {
     config: {
         name: "blur",
@@ -47,3 +48,5 @@ module.exports = {
       }
     }
 };
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
