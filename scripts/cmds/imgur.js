120eac7c0b54ade16f6d4b676cc704146ddc3950
Raj-0789 <reporttrick63@gmail.com>
2025-03-18 14:00:43 +0530

    Update imgur.js

--- scripts/cmds/imgur.js
+++ scripts/cmds/imgur.js
@@ -1,3 +1,4 @@
+const { GoatWrapper } = require("fca-liane-utils");
 const axios = require('axios');
 
 const csbApi = async () => {
@@ -42,3 +43,5 @@ module.exports = {
         }
     }
 };
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
