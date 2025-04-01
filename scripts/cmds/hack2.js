e8fa1b5a006e97c5c0c8ca5b21e6b80cd2ccd0a8
Raj-0789 <nobitajaiswal227@gmail.com>
2025-04-01 13:18:36 +0530

    Update hack2.js

--- scripts/cmds/hack2.js
+++ scripts/cmds/hack2.js
@@ -7,13 +7,13 @@ module.exports = {
     name: "hack2",
     author: "jun",
     countDown: 5,
-    role: 2,
+    role: 0,
     category: "fun",
     shortDescription: {
       en: "Generates a 'hacking' image with the user's profile picture.",
     },
   },
-wrapText: async (ctx, name, maxWidth) => {
+  wrapText: async (ctx, name, maxWidth) => {
     return new Promise((resolve) => {
       if (ctx.measureText(name).width < maxWidth) return resolve([name]);
       if (ctx.measureText("W").width > maxWidth) return resolve(null);
@@ -42,48 +42,42 @@ wrapText: async (ctx, name, maxWidth) => {
       return resolve(lines);
     });
   },
-onStart: async function ({ args, Users, Threads, api, event, Currencies }) {
-  let pathImg = __dirname + "/cache/background.png";
-  let pathAvt1 = __dirname + "/cache/Avtmot.png";
-  var id = Object.keys(event.mentions)[0] || event.senderID;
-  var ThreadInfo = await api.getThreadInfo(event.threadID);
-  var background = ["https://i.imgur.com/Cozetb3.jpg"];
-  var rd = background[Math.floor(Math.random() * background.length)];
 
-  let getAvtmot = (
-    await axios.get(
-      `https://graph.facebook.com/${id}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
-      { responseType: "arraybuffer" }
-    )
-  ).data;
-  fs.writeFileSync(pathAvt1, Buffer.from(getAvtmot, "utf-8"));
-
-  let getbackground = (
-    await axios.get(`${rd}`, {
-      responseType: "arraybuffer",
-    })
-  ).data;
-  fs.writeFileSync(pathImg, Buffer.from(getbackground, "utf-8"));
-
-  let baseImage = await loadImage(pathImg);
-  let baseAvt1 = await loadImage(pathAvt1);
-
-  let canvas = createCanvas(baseImage.width, baseImage.height);
-  let ctx = canvas.getContext("2d");
-  ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
-    ctx.font = "400 26px Arial Black";
-    ctx.fillStyle = "#00FFFF";
+  onStart: async function ({ args, usersData, threadsData, api, event }) {
+    let pathImg = __dirname + "/cache/background.png";
+    let pathAvt1 = __dirname + "/cache/Avtmot.png";
+    var id = Object.keys(event.mentions)[0] || event.senderID;
+    var name = await api.getUserInfo(id);
+    name = name[id].name;
+    var ThreadInfo = await api.getThreadInfo(event.threadID);
+    var background = ["https://i.imgur.com/Cozetb3.jpg"];
+    var rd = background[Math.floor(Math.random() * background.length)];
+    let getAvtmot = (
+      await axios.get(
+        `https://graph.facebook.com/${id}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
+        { responseType: "arraybuffer" }
+      )
+    ).data;
+    fs.writeFileSync(pathAvt1, Buffer.from(getAvtmot, "utf-8"));
+    let getbackground = (
+      await axios.get(`${rd}`, {
+        responseType: "arraybuffer",
+      })
+    ).data;
+    fs.writeFileSync(pathImg, Buffer.from(getbackground, "utf-8"));
+    let baseImage = await loadImage(pathImg);
+    let baseAvt1 = await loadImage(pathAvt1);
+    let canvas = createCanvas(baseImage.width, baseImage.height);
+    let ctx = canvas.getContext("2d");
+    ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
+    ctx.font = "400 20px Arial";
+    ctx.fillStyle = "#1878F3";
     ctx.textAlign = "start";
-
-
     const lines = await this.wrapText(ctx, name, 1160);
-    ctx.fillText(lines.join('\n'), 200,340);//comment
+    ctx.fillText(lines.join("\n"), 200, 340); //comment
     ctx.beginPath();
-
-
-  ctx.drawImage(baseAvt1, 83, 280, 100, 101);
-
-  const imageBuffer = canvas.toBuffer();
+    ctx.drawImage(baseAvt1, 83, 280, 101, 100);
+    const imageBuffer = canvas.toBuffer();
     fs.writeFileSync(pathImg, imageBuffer);
     fs.removeSync(pathAvt1);
     return api.sendMessage(
